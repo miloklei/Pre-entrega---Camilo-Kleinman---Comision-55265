3e2a1443a3b37587449c800e3161cff12df303ef
miloklei <cjkteatro@gmail.com>
2023-07-09 12:49:05 -0300

    Primera pre entrega

---
+++ main.js
@@ -0,0 +1,19 @@
+function saludar() {
+    console.log("¡Bienvenidxs a Adopta un michi!");
+}
+
+saludar()
+
+let nombreIngresado = prompt("Ingrese ID")
+alert("El ID ingresado es " + nombreIngresado)
+
+
+function password() {
+    console.log("¡Bienvenidxs a Adopta un michi!");
+}
+
+password()
+
+let passwordIngresado = prompt("Ingrese Password")
+alert("El Password ingresado es " + passwordIngresado)
+
