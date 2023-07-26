1a02400edc3d57e10f489a108d8ebc1cd525aa21
miloklei <cjkteatro@gmail.com>
2023-07-25 22:21:01 -0300

    Segunda entrega

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function saludar() {
-    console.log("¡Bienvenidxs a Adopta un michi!");
+    console.log("¡Bienvenidxs al Simulador de gastos mensuales!");
 }
 
 saludar()
@@ -23,13 +23,13 @@ while(!nombreIngresado.includes("@"));
 
 
 function password() {
-    console.log("¡Bienvenidxs a Adopta un michi!");
+    console.log("¡Bienvenidxs al Simulador de gastos mensuales!");
 }
 
 password()
 
 let passwordIngresado = prompt("Ingrese Password")
-alert("El Password ingresado es " + passwordIngresado)
+
 
 function registro () {
     console.log("Te registraste correctamente");
@@ -45,4 +45,24 @@ function info (){
 
 info()
 
-let infoDeUsuario =alert("Tus dastos para el ID son" + " " + "Nombre de usuario:" + " " + nombreIngresado + " " + "Password:" + " " + passwordIngresado)
+let infoDeUsuario =alert("Tus datos son" + " " + "Nombre de usuario:" + " " + nombreIngresado + " " + "Password:" + " " + passwordIngresado)
+
+
+function calculateBudget() {
+    const income = parseFloat(document.getElementById("income").value);
+    const expenses = parseFloat(document.getElementById("expenses").value);
+
+    if (isNaN(income) || isNaN(expenses)) {
+        alert("Ingresa números válidos para los ingresos y los gastos.");
+        return;
+    }
+
+    const budget = income - expenses;
+    const resultElement = document.getElementById("result");
+
+    if (budget >= 0) {
+        resultElement.textContent = `Tu presupuesto mensual es positivo: $${budget.toFixed(2)}`;
+    } else {
+        resultElement.textContent = `Tu presupuesto mensual es negativo: $${budget.toFixed(2)}`;
+    }
+}
