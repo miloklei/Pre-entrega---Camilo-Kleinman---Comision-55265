ace84a6ab168aa5d3b1e600dc33904f5b3d35f2f
miloklei <cjkteatro@gmail.com>
2023-08-03 19:52:11 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,68 +1,106 @@
+class Gasto{
+    constructor(titulo, descripcion, monto){
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.monto = parseInt(monto);
+    }    
+}
+
+let primerIngreso= true;
+let nombreIngresado;
+let passwordIngresado;
+let ListaGastos = [];
+let total = 0;
+let mayorGasto = new Gasto();
+
+
+function info(){
+    alert("Estos son tus datos de Usuario");
+    alert("Tus datos son" + " " + "Nombre de usuario:" + " " + nombreIngresado + " " + "Password:" + " " + passwordIngresado);
+}
+
+function pedirUsuario(){
+    do{
+        if (primerIngreso)
+        {
+            nombreIngresado = prompt("Ingrese ID");
+            primerIngreso = false;
+        }
+        else
+        {
+            alert("El ID debe contener el carácter '@'. Por favor, ingréselo nuevamente.");
+            nombreIngresado = prompt("Ingrese ID");
+        }
+    }
+    while(!nombreIngresado.includes("@"));
+}
+
+function password() {
+    console.log("¡Bienvenidxs al Simulador de gastos mensuales!");
+    passwordIngresado = prompt("Ingrese Password")
+    console.log("Te registraste correctamente");
+}
+
 function saludar() {
     console.log("¡Bienvenidxs al Simulador de gastos mensuales!");
 }
 
+function agregar(titulo, descripcion, monto){
+    if(titulo != "" || descripcion != "" || monto != "" ){
+    let nuevoGasto = new Gasto(titulo, descripcion, monto);
+    ListaGastos.push(nuevoGasto);
+    limpiar();
+    alert("Gasto ingresado.");
+    }
+    else{
+        alert("Dato invalido")
+    }
+}
+
+function limpiar(){
+    document.getElementById("titulo").value = "";
+    document.getElementById("descripcion").value = "";
+    document.getElementById("monto").value = parseInt(0);
+}
+
+function finalizar(){
+    if (ListaGastos.length > 0){
+    document.getElementById("botones").style.display = "none";
+    document.getElementById("resultados").style.display = "";
+
+    sumar();
+    encontrarMayorGasto();
+    document.getElementById("gastosTotales").textContent = total;
+    document.getElementById("mayorGasto").textContent = "El mayor gasto es " + mayorGasto.titulo + "- " + mayorGasto.descripcion + ", el monto es de: " + mayorGasto.monto;
+    }
+    else {
+        alert("Ningun gasto fue ingresado");
+    }
+}
+
+function sumar(){
+    ListaGastos.forEach((gasto) => total += gasto.monto);
+    }
+
+function encontrarMayorGasto(){
+    mayorGasto.monto = 0;
+    ListaGastos.forEach ((gasto) => {
+        if(gasto.monto > mayorGasto.monto)
+        { mayorGasto.monto = gasto.monto;
+        mayorGasto.titulo = gasto.titulo;
+        mayorGasto.descripcion = gasto.descripcion}
+    });
+}  
+
+document.getElementById("resultados").style.display = "none";
+
+
 saludar()
-
-let primerIngreso= true;
-let nombreIngresado;
-
-do{
-    if (primerIngreso)
-    {
-        nombreIngresado = prompt("Ingrese ID");
-        primerIngreso = false;
-    }
-    else
-    {
-        alert("El ID debe contener el carácter '@'. Por favor, ingréselo nuevamente.");
-        nombreIngresado = prompt("Ingrese ID");
-    }
-}
-while(!nombreIngresado.includes("@"));
-
-
-function password() {
-    console.log("¡Bienvenidxs al Simulador de gastos mensuales!");
-}
-
+pedirUsuario();
 password()
-
-let passwordIngresado = prompt("Ingrese Password")
-
-
-function registro () {
-    console.log("Te registraste correctamente");
-}
-
-registro()
-
-let registroCorrecto =alert("Te registraste correctamente")
-
-function info (){
-    console.log("Estos son tus datos de Usuario");
-}
-
-info()
-
-let infoDeUsuario =alert("Tus datos son" + " " + "Nombre de usuario:" + " " + nombreIngresado + " " + "Password:" + " " + passwordIngresado)
+info();
 
 
-function calculateBudget() {
-    const income = parseFloat(document.getElementById("income").value);
-    const expenses = parseFloat(document.getElementById("expenses").value);
 
-    if (isNaN(income) || isNaN(expenses)) {
-        alert("Ingresa números válidos para los ingresos y los gastos.");
-        return;
-    }
 
-    const budget = income - expenses;
-    const resultElement = document.getElementById("result");
 
-    if (budget >= 0) {
-        resultElement.textContent = `Tu presupuesto mensual es positivo: $${budget.toFixed(2)}`;
-    } else {
-        resultElement.textContent = `Tu presupuesto mensual es negativo: $${budget.toFixed(2)}`;
-    }
-}
